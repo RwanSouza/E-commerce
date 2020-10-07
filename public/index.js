0a02a033d0d662c087a2ef4e66070fcc59699c1b
Rwan <rwan.souza@hotmail.com>
2020-10-07 19:59:37 -0300

    Upload images logic

--- public/index.js
+++ public/index.js
@@ -13,3 +13,37 @@
 
     }
   }
+
+  const PhotosUpload = {
+    uploadLimit:6,  
+    handleFileInput(event) {
+      const { files: fileList } = event.target;
+      const { uploadLimit } = PhotosUpload;
+
+      if(fileList.length > uploadLimit) {
+        alert("Send max 6 photos");
+        event.preventDefault();
+        return
+      }
+
+      Array.from(fileList).forEach((file) => {
+        const reader = new FileReader();
+
+        reader.onload = () => {
+          const image = new Image();
+          image.src = String(reader.result);
+
+          const container = document.createElement('div');
+          container.classList.add('photo');
+
+          container.onclick = () => alert('Remove photo');
+
+          container.appendChild(image);
+
+          document.querySelector('#photos-preview').appendChild(container);
+        }
+
+        reader.readAsDataURL(file);
+      });
+    }
+  }
