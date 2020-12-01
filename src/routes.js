3d6e517b781566abd36b960ddea8c130140a729e
Rwan <rwan.souza@hotmail.com>
2020-12-01 18:30:38 -0300

    Search page & refactoring section-products

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,16 @@ const routes = express.Router();
 const multer = require('./app/middlewares/multer');
 const productController = require('./app/Controllers/ProductController');
 const homeController = require('./app/Controllers/HomeController');
+const searchController = require('./app/Controllers/SearchController');
 
 
+// Home
 routes.get('/', homeController.index);
 
+// Search
+routes.get('/products/search', searchController.index)
+
+// Products
 routes.get('/products/create', productController.create);
 routes.get('/products/:id', productController.show)
 routes.get('/products/:id/edit', productController.edit);
@@ -16,7 +22,7 @@ routes.put('/products', multer.array("photos", 6), productController.put);
 routes.delete('/products', productController.delete);
 
 
-
+// Alias
 routes.get('/', (req, res ) => {
   return res.redirect('products/create');
 });
