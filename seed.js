9bf310af8a0a42d6ec0b7cbfdb75c7e91fd00bc2
Rwan <rwan.souza@hotmail.com>
2021-02-09 17:50:51 -0300

    Seeds Products

--- seed.js
+++ seed.js
@@ -2,8 +2,11 @@ const faker = require('faker')
 const { hash } = require('bcryptjs')
 
 const User = require('./src/app/models/User')
+const Product = require('./src/app/models/Product')
+const File = require('./src/app/models/File')
 
 let usersIds = []
+let totalProducts = 10
  
 async function createUsers() {
   const users = []
@@ -25,4 +28,44 @@ async function createUsers() {
   usersIds = await Promise.all(usersPromise)
 }
 
-createUsers()
+async  function createProducts() {
+  let products = []
+  
+  while(products.length < totalProducts) {
+    products.push({
+      category_id: Math.ceil(Math.random() * 3),
+      user_id: usersIds[Math.floor(Math.random() * 5)],
+      name:faker.name.title(),
+      description: faker.lorem.paragraph(Math.random() * 10),
+      old_price:faker.random.number(9999),
+      price: faker.random.number(9999),
+      quantity: faker.random.number(99),
+      status:Math.round(Math.random())
+    })
+  }
+
+  const productsPromise = products.map(product => Product.create(product))
+  productsIds = await Promise.all(productsPromise)
+
+  let files = []
+
+  while(files.length < 50 ) {
+    files.push({
+      name:faker.image.image(),
+      path: `public/images/placeholder.png`,
+      product_id: productsIds[Math.floor(Math.random() * totalProducts )]
+    })
+  }
+
+  const filesPromise = files.map(file => File.create(file))
+
+  await Promise.all(filesPromise)
+}
+ 
+
+async function init() {
+  await createUsers()
+  await createProducts()
+}
+
+init()
